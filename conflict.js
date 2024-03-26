36b9e2f7e584de7171fd63ed168810ee6d3a6cd3
prethesh <preethesh.devadiga@codecraft.co.in>
2024-03-26 12:39:36 +0530

    added commit 2

--- conflict.js
+++ conflict.js
@@ -1,1 +1,4 @@
 //created conflict
+/**
+ * When you run git clean, it will delete any untracked files in the working directory, but it won't remove any untracked directories or files specified in .gitignore.
+ */
